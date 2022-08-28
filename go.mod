1951c4db3f1c5c4c8eb45cad18a9f1b1d8cb86fa
Dean <996128+zmes50416@users.noreply.github.com>
2022-08-28 08:46:38 +0000

    WIP: http2 server

---
+++ go.mod
@@ -0,0 +1,7 @@
+module exp
+
+go 1.19
+
+require golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b
+
+require golang.org/x/text v0.3.7 // indirect
--- server/server.go
+++ server/server.go
@@ -3,17 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
 	log.Println("Server starting...")
-	log.Fatal(http.ListenAndServe(":8888", http.HandlerFunc(limitHandler)))
+	h2s := &http2.Server{}
+	server := &http.Server{
+		Addr:    "0.0.0.0:8888",
+		Handler: h2c.NewHandler(http.HandlerFunc(limitHandler), h2s),
+	}
+	log.Fatal(server.ListenAndServe())
 }
 
 func limitHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Body == nil {
+	data := make([]byte, 128)
+	if _, err := r.Body.Read(data); err == io.EOF {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		return
+	} else if err != nil {
+		log.Println("Read request body failed, ", err)
+		return
 	}
 	
 	w.WriteHeader(http.StatusRequestEntityTooLarge)
