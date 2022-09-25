39b38436e63258358e275d8dc1ab39d17ae67793
Dean <996128+zmes50416@users.noreply.github.com>
2022-09-25 09:14:07 +0000

    Add java client code template

---
+++ clients/java/src/main/java/com/example/demo/config/HttpConfig.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpConfig {
+
+    @Bean
+    public void okHttpClient() {
+        
+    }
+}
